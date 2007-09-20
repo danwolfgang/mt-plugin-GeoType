3f504523fcd21c12e862ba762eeae24888bf0244
Mike Kania <mkania@sixapart.com>
2007-09-20 15:33:06 +0000

    upping version number

--- plugins/GeoType/geo_type.pl
+++ plugins/GeoType/geo_type.pl
@@ -46,7 +46,7 @@ use GeoType::EntryLocation;
 use Data::Dumper;
 
 use vars qw( $VERSION );
-$VERSION = 1.3; 
+$VERSION = 1.4; 
 
 my $plugin = MT::Plugin::GeoType->new ({
     name        => "GeoType",
