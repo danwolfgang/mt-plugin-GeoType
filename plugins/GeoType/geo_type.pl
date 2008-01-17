7fef14b8f6829166b509e16296ef398c8b5aebe6
Steve Cook <scook@sixapart.com>
2008-01-17 19:09:35 +0000

    Added interface for editing extended attributes (and location name) from GeoType location list

--- plugins/GeoType/geo_type.pl
+++ plugins/GeoType/geo_type.pl
@@ -40,6 +40,7 @@ use strict;
 use warnings;
 
 use MT;
+use MT::ConfigMgr;
 use GeoType::Location;
 use GeoType::EntryLocation;
 use GeoType::ExtendedLocation;
@@ -58,7 +59,7 @@ my $plugin = MT::Plugin::GeoType->new ({
 	author_link => "http://apperceptive.com/",
 
 	schema_version => 1.2,
-	 object_classes => [ 'GeoType::Location', 'GeoType::EntryLocation' ],
+	object_classes => [ 'GeoType::Location', 'GeoType::EntryLocation', 'GeoType::ExtendedLocation' ],
 	
 	system_config_template  => 'config.tmpl',
 	blog_config_template    => 'blog_config.tmpl',
@@ -106,6 +107,7 @@ my $plugin = MT::Plugin::GeoType->new ({
 	app_methods => {
 	   'MT::App::CMS'  => {
 		   'geotype_list_locations'  => \&list_locations,
+		   'geotype_edit_location'  => \&edit_extended_location,
 	   },
 	},
 	
@@ -610,6 +612,58 @@ sub _get_api_key {
 	return $blog_value && $blog_value ne uc($key . '_api_key') ? $blog_value : $system_value ne uc($key . '_api_key') ? $system_value : undef;
 }
 
+sub edit_extended_location {
+	my ($app) = @_;
+        my $param = { };
+	my $plugin = &instance;
+
+	my $blog_id = $app->param('blog_id');
+	my $location_id = $app->param('location_id');
+	$param->{blog_id} = $blog_id;
+	$param->{location_id} = $location_id;
+
+	( $location_id ) or die "Location ID not provided\n";
+	# We need a locaton, but we may not have extended attributes
+	my $location = GeoType::Location->load( $location_id );
+	( $location ) or die "Cannot load location ID $location_id\n";
+	my ( $extended ) = GeoType::ExtendedLocation->load({ location_id => $location_id });
+	if ( $app->param('dosave') ) {
+		$app->param('name') && $location->name($app->param('name'));
+		$extended = GeoType::ExtendedLocation->new unless ( $extended );
+		$extended->location_id($location_id);
+		$extended->cross_street($app->param('cross_street'));
+		$extended->description($app->param('description'));
+		$extended->hours($app->param('hours'));
+		$extended->phone_number($app->param('phone_number'));
+		$extended->place_id($app->param('place_id'));
+		my $rating = $app->param('rating');
+		$rating = "" unless ( $rating eq $rating + 0 );
+		$extended->rating($rating);
+		$extended->thumbnail($app->param('thumbnail'));
+		$extended->url($app->param('url'));
+		$location->save;
+		$extended->save;
+	}
+
+	$param->{loc_name} = $location->name;
+	if ( $extended ) {
+		$param->{loc_description} = $extended->description;
+		$param->{loc_cross_street} = $extended->cross_street;
+		$param->{loc_hours} = $extended->hours;
+		$param->{loc_phone_number} = $extended->phone_number;
+		$param->{loc_place_id} = $extended->place_id;
+		$param->{loc_rating} = $extended->rating;
+		$param->{loc_thumbnail} = $extended->thumbnail;
+		$param->{loc_url} = $extended->url;
+	}
+
+	$app->{breadcrumbs} = [];
+	$app->add_breadcrumb('Edit Location');
+	$param->{script_url} = MT::ConfigMgr->instance->CGIPath . MT::ConfigMgr->instance->AdminScript;
+	my $tmpl = $plugin->load_tmpl("geotype_edit_extended.tmpl");
+        $app->build_page($tmpl, $param);
+}
+
 sub list_locations {
 	my ($app) = @_;
 	
@@ -636,6 +690,7 @@ sub list_locations {
 		Template    => $plugin->load_tmpl ('list_geotype_location.tmpl'),
 		Params  => {
 			quick_search    => 0,
+			extensions      => $plugin->get_config_value ('use_extended_attributes', 'blog:'. $app->blog->id),
 			google_api_key  => $plugin->get_google_api_key ($app->blog),
 			map_height      => $plugin->get_config_value ('map_height', 'blog:'. $app->blog->id),
 			map_width       => $plugin->get_config_value ('map_width', 'blog:' . $app->blog->id),
