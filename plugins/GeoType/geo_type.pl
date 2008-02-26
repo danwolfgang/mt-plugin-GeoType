861a767a1a8191f79d6a0e079197fe92cfb896de
Steve Cook <scook@sixapart.com>
2008-02-26 21:56:50 +0000

    Bumping version to 1.6.3 -- sole feature change is to support MT3.3 in addition to MTE

--- plugins/GeoType/geo_type.pl
+++ plugins/GeoType/geo_type.pl
@@ -48,7 +48,7 @@ use GeoType::ExtendedLocation;
 use Data::Dumper;
 
 use vars qw( $VERSION );
-$VERSION = '1.6.2'; 
+$VERSION = '1.6.3'; 
 
 my $plugin = MT::Plugin::GeoType->new ({
 	name        => "GeoType",
