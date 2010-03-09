bde2ba244fc06f2440bafd4e0fed88ce860d4d2b
Finn Smith <finn@timeghost.net>
2010-03-09 17:31:25 -0500

    Clean up license and copyright notice header comment.
     - Fixed spacing.
     - Removed spurious file/date/committer line.

--- plugins/GeoType/lib/GeoType/Tags.pm
+++ plugins/GeoType/lib/GeoType/Tags.pm
@@ -5,11 +5,10 @@
 # by the Free Software Foundation, or (at your option) any later version.
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-# version 2 for more details.  You should have received a copy of the GNU
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# version 2 for more details. You should have received a copy of the GNU
 # General Public License version 2 along with this program. If not, see
 # <http://www.gnu.org/licenses/>.
-# CMS.pm 20010-03-08  nataliepo
 
 package GeoType::Tags;
 
